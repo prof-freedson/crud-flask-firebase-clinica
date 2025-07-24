4aec1e0f131bb83efc885d9753ff5fe038788ce7
prof-freedson <freedson.prof@gmail.com>
2025-07-24 02:55:20 -0300

    Preparação para PWA

---
+++ static/service-worker.js
@@ -0,0 +1,4 @@
+/*
+  Este arquivo service-worker.js define o comportamento do Service Worker, que é responsável por gerenciar a instalação, atualização e cache de recursos.
+  Consulte o tutorial TUTORIAL-PWA.md para mais detalhes sobre cada campo.
+*/
